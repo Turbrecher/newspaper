daacb8412c03ee3faf088b675e2c5fc113f42d35
Victor Vera <victorveracoronel@live.com>
2024-09-10 10:52:48 +0200

    I've created the initial structure of newspaper.
    3 modules: admin, shared and writers.
    Each one of them has its own services, pages and components.
    
    I will probably add a new module called moderators in the near future.

---
+++ src/app/shared/components/form/label/label.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-label',
+  standalone: true,
+  imports: [],
+  templateUrl: './label.component.html',
+  styleUrl: './label.component.sass'
+})
+export class LabelComponent {
+
+}
---
+++ src/app/shared/pages/news-feed/news-feed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-feed',
+  standalone: true,
+  imports: [],
+  templateUrl: './news-feed.component.html',
+  styleUrl: './news-feed.component.sass'
+})
+export class NewsFeedComponent {
+
+}
